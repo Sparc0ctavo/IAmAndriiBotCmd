cda43d21ccf6201a07107d4e3e4bc1eb7b6bc4ea
Andriy <malyavsky4@gmail.com>
2025-04-25 21:30:53 +0200

    Add project files.

---
+++ IAmAndriiBotCmd/IAmAndriiBot.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace IAmAndriiBotCmd
+{
+    internal class IAmAndriiBot
+    {
+
+        private bool _isRunning;
+        private TelegramBotClient _client;
+        private CancellationTokenSource _cts;
+        private string _bot_token;
+
+        public IAmAndriiBot()
+        {
+            _isRunning = true;
+            _bot_token = "7848368901:AAGYOzdhRmjU010o0bHvu4pzhVnSAjfXuUM";
+            _cts = new CancellationTokenSource();
+            _client = new TelegramBotClient(_bot_token, cancellationToken: _cts.Token);
+
+            var commands = new[]
+            {
+                new BotCommand { Command = "start", Description = "почати роботу"},
+                new BotCommand { Command = "help", Description = "список всіх команд"},
+                new BotCommand { Command = "presentation", Description = "презентувати автора"},
+                new BotCommand { Command = "cv", Description = "резюме"},
+                new BotCommand { Command = "contacts", Description = "контакти"}
+            };
+
+            _client.SetMyCommands(commands);
+
+        }
+
+        public void Start()
+        {
+
+            _client.OnMessage += OnMessage;
+
+            Console.WriteLine($"Бот запущено! Натисніть CTRL+C, щоб завершити роботу.");
+
+            while (_isRunning) { Task.Delay(1000); }
+            Stop();
+        }
+
+        public void Stop() 
+        {
+            Console.ReadKey();
+            _cts.Cancel();
+        }
+
+        private async Task OnMessage(Message message, UpdateType updateType)
+        {
+            int scriptId = -1;
+            string textRespose = GetTextResponseScripts(scriptId);
+
+            if (message.Type == MessageType.Text && !string.IsNullOrEmpty(message.Text))
+            {
+                if (message.Text.StartsWith("/start"))
+                {
+                    scriptId = 0;
+                    textRespose = GetTextResponseScripts(scriptId);
+                }
+                else if (message.Text.StartsWith("/help"))
+                {
+                    scriptId = 1;
+                    textRespose = GetTextResponseScripts(scriptId);
+                }
+                else if (message.Text.StartsWith("/present"))
+                {
+                    scriptId = 2;
+                    textRespose = GetTextResponseScripts(scriptId);
+                }
+                else if (message.Text.StartsWith("/cv"))
+                {
+                    scriptId = 3;
+                    textRespose = GetTextResponseScripts(scriptId);
+                }
+
+                try
+                {
+                    await _client.SendMessage(message.Chat.Id, textRespose, parseMode: ParseMode.Markdown);
+                    
+                    Console.WriteLine($"Повідомлення номер \"{scriptId}\" до {message.Chat} надіслано!");
+
+                    if(scriptId == 3) 
+                    {
+                        var filePath = "C:\\Users\\malya\\source\\repos\\IAmAndriiBotCmd\\IAmAndriiBotCmd\\Content\\CV-Andrii.pdf";
+
+                        if (File.Exists(filePath))
+                        {
+                            await _client.SendDocument(message.Chat.Id, filePath);
+                            Console.WriteLine($"Файл {filePath} успішно надіслано");
+                        }
+                        else { Console.WriteLine("Такого файлу не існує."); }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+
+        private string GetTextResponseScripts(int scriptId)
+        {
+            if (scriptId == 0)
+            {
+                return "Привіт! Я бот, створений в рамках завдання від DICEUS. Тут можна дізнатися коротку інформацію про автора. Для допомоги в навігації по меню введіть /help. Бажаю гарного \"клацання\"!\u2665";
+            }
+            else if (scriptId == 1)
+            {
+                return "Ось команди для навігації:\n/help - список всіх команд.\n/presentation - презентувати автора.\n/cv - резюме.\n/contacts - контакти.";
+            }
+            else if (scriptId == 2)
+            {
+                return "Доброго дня! Мене звати Хрущ Андрій Андрійович. Народився 17 вересня 2005 року (19 років) у місті [Тернопіль](https://uk.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%BD%D0%BE%D0%BF%D1%96%D0%BB%D1%8C), Україна." +
+                       " Наразі проживаю в місті [Карпі](https://uk.wikipedia.org/wiki/%D0%9A%D0%B0%D1%80%D0%BF%D1%96), Італія. " +
+                       " З дитинства цікавився відео іграми, музикою, поезією та різною комп'ютерною технікою. З 15 років серйозно(наскільки міг в 15 років) взявся за програмування. Вільно(майже, хах) спілкуюсь англійською, вільно(без \"майже\") українською, та поки що на середньому рівні італійською.";
+            }
+            else if (scriptId == 3)
+            {
+                return "Завантажую резюме...";
+            }
+
+            return "Під час обробки відповіді сталася помилка. Перевірте правильність написаної команди.";
+        }
+
+        private void OnCanselKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            _isRunning = false;
+        }
+
+    }
+}
---
+++ IAmAndriiBotCmd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace IAmAndriiBotCmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            IAmAndriiBot bot = new IAmAndriiBot();
+
+            bot.Start();
+        }
+    }
+}
