72f9b07f954de5319c07dc1e24a93618eb7b0251
Andriy <malyavsky4@gmail.com>
2025-04-26 14:01:42 +0200

    flyedits

--- IAmAndriiBotCmd/IAmAndriiBot.cs
+++ IAmAndriiBotCmd/IAmAndriiBot.cs
@@ -22,7 +22,7 @@ namespace IAmAndriiBotCmd
         public IAmAndriiBot()
         {
             _isRunning = true;
-            _bot_token = "7126653896:AAEnrMWKPAKaW8hb4Mnjpxjb6RkzsOY5pxM" /*Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")*/;
+            _bot_token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
             _cts = new CancellationTokenSource();
             _client = new TelegramBotClient(_bot_token, cancellationToken: _cts.Token);
 
